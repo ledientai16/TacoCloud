403edd33949285a07c3e07d241bcc9631478ecd9
LDT <ledientai16@gmail.com>
2024-01-24 23:33:07 +0700

    TO LEARN SPRING IN ACTION

---
+++ src/test/java/org/idk/tacocloud/HomeControllerTests.java
@@ -0,0 +1,28 @@
+package org.idk.tacocloud;
+
+import org.hamcrest.CoreMatchers;
+import org.idk.tacocloud.controller.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        CoreMatchers.containsString("Welcome to")
+                ));
+    }
+
+}
